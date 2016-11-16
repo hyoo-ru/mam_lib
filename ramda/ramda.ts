b8e1817a79515fe2980a1d578f8f495c8266a007
nin-jin <nin-jin@ya.ru>
2016-11-16 12:52:21 +0300

    Use namespace instead of module.

--- ramda/ramda.ts
+++ ramda/ramda.ts
@@ -1,6 +1,6 @@
 /// Install: 
 ///     npm install ramda
-module $ {
+namespace $ {
 	declare function require( path : string ) : any
 	export let $lib_ramda = $mol_global.R || require( '../../node_modules/ramda/dist/ramda.js' )
 }
