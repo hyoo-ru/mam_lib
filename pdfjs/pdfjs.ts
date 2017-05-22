c7f39590b79fa0eddea053116352d1f7745c85b7
nin-jin <nin-jin@ya.ru>
2017-05-22 18:56:06 +0300

    Deploy pdfjs web worker on building and use it.

--- pdfjs/pdfjs.ts
+++ pdfjs/pdfjs.ts
@@ -3,4 +3,5 @@
 namespace $ {
 	export let $lib_pdfjs = $node['pdfjs-dist/build/pdf.min.js'].PDFJS
 	$lib_pdfjs.disableRange = true
+	$lib_pdfjs.workerSrc = '-/node_modules/pdfjs-dist/build/pdf.worker.min.js'
 }
