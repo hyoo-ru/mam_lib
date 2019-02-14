79870c350e97e907ad89405312cf05857bae1b52
КарловскийДА <kard@1c.ru>
2019-02-14 13:35:42 +0300

    new api support

--- pdfjs/pdfjs.ts
+++ pdfjs/pdfjs.ts
@@ -1,7 +1,7 @@
 /// Install: 
 ///     npm install pdfjs-dist
 namespace $ {
-	export let $lib_pdfjs = $node['pdfjs-dist/build/pdf.min.js'].PDFJS
+	export let $lib_pdfjs = $node['pdfjs-dist/build/pdf.min.js']
 	$lib_pdfjs.disableRange = true
-	$lib_pdfjs.workerSrc = '-/node_modules/pdfjs-dist/build/pdf.worker.min.js'
+	$lib_pdfjs.GlobalWorkerOptions.workerSrc = '-/node_modules/pdfjs-dist/build/pdf.worker.min.js'
 }
