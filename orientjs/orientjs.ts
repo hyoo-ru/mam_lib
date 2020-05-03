7eee86f27720fee17b1810de23e9070c8ccbdf6e
nin-jin <nin-jin@ya.ru>
2020-05-03 21:47:08 +0300

    orientjs: better types

--- orientjs/orientjs.ts
+++ orientjs/orientjs.ts
@@ -2,4 +2,12 @@
 ///     npm install orientjs @types/orientjs
 namespace $ {
 	export let $lib_orientjs : typeof import( 'orientjs' ) = require( 'orientjs/lib/index.js' )
+
+	type Classes = $mol_type_pick< typeof $lib_orientjs , { new( ... args : never[] ): unknown } >
+	type Other = $mol_type_omit< typeof $lib_orientjs , { new( ... args : never[] ): unknown } >
+
+	export type $lib_orientjs = 
+		& { [ name in keyof Classes ] : InstanceType< Classes[ name ] > }
+		& Other
+
 }
