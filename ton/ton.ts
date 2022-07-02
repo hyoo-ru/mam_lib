1a03d4712584aba2756d49efaebd663d5d08f4aa
Pavel Zubkov <zubkov_p@mail.ru>
2022-07-02 07:58:04 +0300

    : Add min.js

--- ton/ton.ts
+++ ton/ton.ts
@@ -1,34 +1,7 @@
 namespace $ {
-	const { default: TonWeb }= require('tonweb') as typeof import('tonweb')
+	// err: Cannot use import statement outside a module
+	// const { default: TonWeb }= require('tonweb') as typeof import('tonweb')
 
-	export class $lib_ton extends $mol_object2 {
+	export const $lib_ton = require('./min.js') as typeof import('tonweb').default
 
-		static api_key() {
-			return 'c0e4df33e6b63b8641add66d09d7aab5fdaef6c87d8afa30fa43c77f9ee51586'
-		}
-
-		@ $mol_mem
-		static api() {
-			return new TonWeb( new TonWeb.HttpProvider('https://testnet.toncenter.com/api/v2/jsonRPC', { apiKey: this.api_key() }) );
-		}
-
-		static createKeys() {
-			return TonWeb.utils.nacl.sign.keyPair();
-		}
-
-		@ $mol_action
-		static createWallet(keys: ReturnType<typeof $lib_ton.createKeys>) {
-			const tonweb = new TonWeb();
-
-			const WalletClass = tonweb.wallet.all.v3R2;
-			const wallet = new WalletClass(tonweb.provider, { publicKey: keys.publicKey });
-
-			return wallet
-		}
-		// https://github.com/toncenter/examples/blob/main/deposits-single-wallet.js
-		// https://github.com/toncenter/tonweb/blob/master/src/contract/wallet/README.md
-		// https://ton.org/docs/#/howto/payment-processing
-		// https://github.com/toncenter/payment-channels-example/blob/main/index.js
-
-	}
 }
