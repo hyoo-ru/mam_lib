9ee8a75fc451f8f42c069b597dbcf05bc8549763
Pavel Zubkov <zubkov_p@mail.ru>
2022-07-02 08:18:41 +0300

    ton

--- ton/ton.ts
+++ ton/ton.ts
@@ -1,7 +1,6 @@
 namespace $ {
-	// err: Cannot use import statement outside a module
-	// const { default: TonWeb }= require('tonweb') as typeof import('tonweb')
 
-	export const $lib_ton = require('./min.js') as typeof import('tonweb').default
+	const { default: TonWeb }= require('tonweb') as typeof import('tonweb')
+	export const $lib_ton = TonWeb
 
 }
