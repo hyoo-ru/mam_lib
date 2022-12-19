387367f0337edfc6c7f5109b9860580c101318ec
Pavel Zubkov <zubkov_p@mail.ru>
2022-12-19 09:00:07 +0300

    +1

--- d3/d3.ts
+++ d3/d3.ts
@@ -4,7 +4,7 @@ namespace $ {
 
 		@ $mol_mem
 		static all() {
-			return $mol_import.script('https://cdn.jsdelivr.net/npm/d3@7').d3 as typeof import('d3') as any
+			return $mol_import.script('https://cdn.jsdelivr.net/npm/d3@7').d3 as any
 		}
 
 	}
